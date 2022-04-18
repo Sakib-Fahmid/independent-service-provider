7d7203fcc089d74bfb572bd83047a0ffa82a96e9
Fahmid Hossain <fahmidhossainsakib@gmail.com>
2022-04-18 22:32:18 +0600

    404 done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
 import Helmet from 'react-helmet';
 import Login from './Components/Login/Login';
+import Notfound from './Components/Notfound/Notfound';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='*' element={<Notfound></Notfound>}></Route>
       </Routes>
       <Footer></Footer>
     </>
